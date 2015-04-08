ef6bcad8bb51d19da76c6fc499712cb8df8ebe1e
Joe Schaul <joe.schaul@gmail.com>
2015-04-08 15:23:25 +0200

    only accept registration when terms and conditions are checked.

--- module-code/app/securesocial/controllers/Registration.scala
+++ module-code/app/securesocial/controllers/Registration.scala
@@ -73,7 +73,7 @@ trait BaseRegistration[U] extends MailTokenBasedOperations[U] {
         ).verifying(Messages(PasswordsDoNotMatch), passwords => passwords._1 == passwords._2),
       Email -> email.verifying(nonEmpty),
         CheckNewsletter -> boolean,
-      CheckAcceptTermsAndConditions -> boolean
+      CheckAcceptTermsAndConditions -> checked("securesocial.signup.error.invalidTermsAndConditions")
     ) // binding
     ((userName, firstName, lastName, password, email, checkGetInfo, checkAgreement) => RegistrationInfo(Some(userName), firstName, lastName, password
         ._1,
@@ -92,7 +92,7 @@ trait BaseRegistration[U] extends MailTokenBasedOperations[U] {
         ).verifying(Messages(PasswordsDoNotMatch), passwords => passwords._1 == passwords._2),
       Email -> email.verifying(nonEmpty),
       CheckNewsletter -> boolean,
-      CheckAcceptTermsAndConditions -> boolean
+      CheckAcceptTermsAndConditions -> checked("securesocial.signup.error.invalidTermsAndConditions")
     ) // binding
     ((firstName, lastName, password, email, checkGetInfo, checkAgreement) => RegistrationInfo(None, firstName, lastName, password._1, email, checkGetInfo, UserAgreement(checkAgreement))) //
     // unbinding
